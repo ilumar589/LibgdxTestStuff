83c963ad80b8cedb0a699952a75410885ff2ec07
Parvu Eduard <parvueduard@gmail.com>
2020-10-19 23:42:37 +0300

    Initial commit

---
+++ core/src/main/java/org/test/stuff/TestStuffMain.java
@@ -0,0 +1,7 @@
+package org.test.stuff;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class TestStuffMain extends ApplicationAdapter {
+}
---
+++ lwjgl3/src/main/java/org/test/stuff/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,24 @@
+package org.test.stuff.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import org.test.stuff.TestStuffMain;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static void createApplication() {
+		new Lwjgl3Application(new TestStuffMain(), getDefaultConfiguration());
+	}
+
+	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+		configuration.setTitle("LibgdxTestStuff");
+		configuration.setWindowedMode(640, 480);
+		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+		return configuration;
+	}
+}

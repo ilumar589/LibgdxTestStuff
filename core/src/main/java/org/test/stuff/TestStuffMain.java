1308e8cd24f5be98871077cccf9c72de5b4fe0c2
Parvu Eduard <parvueduard@gmail.com>
2020-10-22 22:49:00 +0300

    example + use ZGC

--- core/src/main/java/org/test/stuff/TestStuffMain.java
+++ core/src/main/java/org/test/stuff/TestStuffMain.java
@@ -1,7 +1,29 @@
 package org.test.stuff;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class TestStuffMain extends ApplicationAdapter {
+
+    private SpriteBatch batch;
+    private Texture img;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
 }
